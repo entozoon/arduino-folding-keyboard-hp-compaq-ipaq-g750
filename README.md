dd81d3895ee0a497cf3459de7b2717a8017fb9e6
Mickey <michael.cook@techdept.co.uk>
2021-11-25 22:55:11 +0000

    Spitting out serial from the keyboard, woop!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-arduino-folding-keyboard-hp-compaq-ipaq-g750
+# Arduino Folding Keyboard (HP Compaq Ipaq G750)
+
+    Keyboard ➡ RS232<->TTL MAX232 Module ➡ Arduino
+
+(All 3.3V)
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,17 @@
 #include <Arduino.h>
-const int pinTestsLength = 5;
-const int pinTests[pinTestsLength] = {LED_BUILTIN, 0, 1, 2, 3};
+#include <SoftwareSerial.h>
+SoftwareSerial keySerial(2, 3); // RX (D2), TX
 void setup()
 {
-  Serial.begin(115200); // 76800 is good for esp, as it boots in this. need to update serial monitor bitch
-  // Spews gargage in serial monitor after upload but.. should calm down and work; certainly after a reset. No idea why (might just be platformio)
-  delay(4000); // Pause for upload issues
-  Serial.println("\nLet's go!");
-  for (int i = 0; i < pinTestsLength; i++)
-  {
-    Serial.print("Init ");
-    Serial.println(pinTests[i]);
-    pinMode(pinTests[i], OUTPUT);
-  }
+  Serial.begin(115200);
+  keySerial.begin(4800);
+  keySerial.listen();
+  delay(2000);
 }
 void loop()
 {
-  for (int i = 0; i < pinTestsLength; i++)
+  if (keySerial.available())
   {
-    Serial.print("Testing pin code ");
-    Serial.println(pinTests[i]);
-    digitalWrite(pinTests[i], 1);
-    delay(1500);
-    digitalWrite(pinTests[i], 0);
+    String keyData = String(keySerial.read(), HEX);
+    Serial.println(keyData);
   }
-  Serial.println("");
-}
